2d99ecccab2693dcee89c6fe23a93c0b69a1ea66
Sergey Kryukov <krus-83@mail.ru>
2021-03-28 20:30:43 +0700

    preview file info

--- upload.js
+++ upload.js
@@ -1,6 +1,18 @@
+function byToSize(bytes) {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  if (!bytes) {
+    return '0 Byte';
+  }
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log((1024))));
+  return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i];
+}
+
 export function upload(selector, options = {}) {
   const input = document.querySelector(selector);
   const btnOpen = document.createElement('button');
+  const preview = document.createElement('div');
+
+  preview.classList.add('preview');
 
   btnOpen.classList.add('btn');
   btnOpen.textContent = 'Open';
@@ -15,6 +27,7 @@ export function upload(selector, options = {}) {
     input.setAttribute('accept', extsList)
   }
 
+  input.insertAdjacentElement('afterend', preview);
   input.insertAdjacentElement('afterend', btnOpen);
 
   const triggerInputFile = () => input.click();
@@ -26,14 +39,33 @@ export function upload(selector, options = {}) {
 
     const files = Array.from(event.target.files);
 
+    preview.innerHTML = '';
+
     files.forEach(file => {
       if (!file.type.match('image')) {
         return;
       }
 
+      const reader = new FileReader();
 
+      reader.onload = ev => {
+        const itemHTML = `
+          <div class="preview-image">
+            <div class="preview-image__remove">&times;</div>
+            <img src= "${ev.target.result}" alt="${file.name}"/>
+            <div class="preview-image__info">
+              <span>${file.name}</span>
+              <span>${byToSize(file.size)}</span>
+            </div>
+          </div>
+        `;
+
+        preview.insertAdjacentHTML('afterbegin', itemHTML);
+      };
+
+      reader.readAsDataURL(file); // асинхронная операция
     });
-  }
+  };
 
   btnOpen.addEventListener('click', triggerInputFile);
   input.addEventListener('change', changeHandler);
